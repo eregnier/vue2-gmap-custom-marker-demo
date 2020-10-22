0cc53cb82c506b681700c1b537a45dd6506adb8c
Eric Régnier <eric.regnier@ext.leroymerlin.fr>
2020-10-22 14:52:21 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import App from "./App.vue";
+import * as VueGoogleMaps from "vue2-google-maps";
+// Using Cluster requires marker-clusterer-plus to be installed.
+import GmapCluster from "vue2-google-maps/dist/components/cluster";
+
+Vue.component("cluster", GmapCluster);
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key : process.env.VUE_APP_GMAP_API_KEY,
+    v: "3.26"
+  },
+  installComponents: false
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
