5a8cbc16f2ec4e9bd5f1837259dbf7beeacf33a9
eric regnier <utopman@gmail.com>
2020-10-27 21:03:13 +0100

    add gmap key in CI

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
 import * as VueGoogleMaps from "vue2-google-maps";
+
+import App from "./App.vue";
 // Using Cluster requires marker-clusterer-plus to be installed.
 import GmapCluster from "vue2-google-maps/dist/components/cluster";
+import Vue from "vue";
 
 Vue.component("cluster", GmapCluster);
-
+console.log('k', process.env.VUE_APP_GMAP_API_KEY)
 Vue.use(VueGoogleMaps, {
   load: {
-    key : process.env.VUE_APP_GMAP_API_KEY,
+    key: process.env.VUE_APP_GMAP_API_KEY,
     v: "3.26"
   },
   installComponents: false
